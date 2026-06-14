a787f6b49096643475d19f57a3f42a2b56e51811
52Blue05 <trinhnam30092005@gmail.com>
2026-06-14 11:45:58 +0700

    feat: initialize Spring Boot project structure with Gradle wrapper

---
+++ src/main/java/com/hrc/runnertracker/RunnertrackerApplication.java
@@ -0,0 +1,13 @@
+package com.hrc.runnertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnertrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnertrackerApplication.class, args);
+	}
+
+}
